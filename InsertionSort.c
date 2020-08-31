37df12389f3609e68810a81f1a4324330e35bee9
Pranav Tanpure <pranav280300@gmail.com>
2020-08-31 22:32:31 +0530

    initial commmit

---
+++ InsertionSort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+void getdata(int *p,int n){
+    for(int i=0;i<n;i++)
+    scanf("%d",(p+i));
+
+}
+void sort(int *p,int n)
+{
+    int i,key,j;
+    for( i=1;i<n;i++){
+     key=p[i];
+    j=i-1;
+    while(j>=0 && p[j]>key){
+        p[j+1]=p[j];
+        j=j-1;
+    }
+    p[j+1]=key;
+    }
+
+}
+void printoutput(int *p,int n){
+    for(int i=0;i<n;i++)
+    printf("%d ",p[i]);
+}
+void main(){
+    int *p;
+    int n;
+   
+    printf("Enter the size of array \n");
+    scanf("%d",&n);
+    p=(int *)malloc(n*sizeof(int));
+    printf("Enter the data to be sorted \n");
+    getdata(p,n);
+    sort(p,n);
+    printf("Sorted ... \n");
+    printoutput(p,n);
+}
